f262cc3613b17d00ab4dae22bf56f3d9857f9425
schaslivchikm <mikhahil.shapovalov@xloo.com>
2018-08-02 09:35:54 +0300

    code for preview image

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-testing();
+var file = document.querySelector('[type="file"]')
 
-function testing(){
-    console.log('testing')
+file.addEventListener('change', function(){
+    previewImage(this);
+})
+
+function previewImage(input) {
+    if(input.files && input.files[0]){
+        var reader = new FileReader();
+
+        reader.onload = function(e){
+            var image = document.createElement('img')
+            image.src = e.target.result;
+
+            input.parentNode.insertBefore(image, input)
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    } 
 }
-
-var title = document.getElementById('title');
-title.innerText = "git is hell";
